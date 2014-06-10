8b104797281c7614c2d1661ac0189ebad5e17b96
Terry Worona <terryworona@gmail.com>
2014-06-09 22:45:18 -0700

    Flushing out main controller view

--- anscombe-quartet-ios/Controllers/AQChartViewController.m
+++ anscombe-quartet-ios/Controllers/AQChartViewController.m
@@ -8,7 +8,33 @@
 
 #import "AQChartViewController.h"
 
-@interface AQChartViewController ()
+// Views
+#import <JBLineChartView.h>
+
+@protocol AQScrollViewDataSource;
+@protocol AQScrollViewDelegate;
+
+@interface AQChartScrollView : UIScrollView
+
+@property (nonatomic, weak) id<AQScrollViewDataSource> scrollDataSource;
+@property (nonatomic, weak) id<AQScrollViewDelegate> scrollDelegate;
+
+- (void)reloadData;
+
+@end
+
+@protocol AQScrollViewDataSource <NSObject>
+
+@end
+
+@protocol AQScrollViewDelegate <NSObject>
+
+@end
+
+@interface AQChartViewController () <AQScrollViewDataSource, AQScrollViewDelegate>
+
+@property (nonatomic, strong) AQChartScrollView *scrollView;
+@property (nonatomic, strong) NSArray *chartViews;
 
 @end
 
@@ -16,6 +42,24 @@
 
 #pragma mark - View Lifecycle
 
+- (void)loadView
+{
+    [super loadView];
+    
+    self.scrollView = [[AQChartScrollView alloc] initWithFrame:CGRectMake(self.view.bounds.origin.x, self.view.bounds.origin.y, self.view.bounds.size.width, 200)];
+    self.scrollView.scrollDataSource = self;
+    self.scrollView.scrollDelegate = self;
+    [self.tableView setTableHeaderView:self.scrollView];
+}
 
+
+@end
+
+@implementation AQChartScrollView
+
+- (void)reloadData
+{
+    
+}
 
 @end
